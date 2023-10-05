f2e52359b6b4e55e316c7fd8ebe139e95541a2f5
Shaban-mern-developer <shabanamerndeveloper@gmail.com>
2023-10-05 11:53:31 -0700

    changes

--- routes/todosRoute.js
+++ routes/todosRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const Todo = require('../models/todos')
 
 
-app.get('/todos', async (req, res) => {
+router.get('/todos', async (req, res) => {
     const todosList = await Todo.find();
     if (todosList) {
         res.json(todosList).status(200)
